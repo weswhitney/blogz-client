e858c01e1902efa682cae01b21bcb696e2bee95b
weswhitney <wesleywhitney@gmail.com>
2015-02-11 10:08:06 -0700

    initial commit with routes controllers models

---
+++ app/models/post.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  body: DS.attr('string'),
+  comments: DS.hasMany('comment'),
+  user: DS.belongsTo('user')
+});
